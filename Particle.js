c49ce75de94ce6f1a72077f1bc729e260b46f2f2
Sidak1311 <67144572+Sidak1311@users.noreply.github.com>
2020-11-02 21:04:56 +0530

    Add files via upload

---
+++ Particle.js
@@ -0,0 +1,19 @@
+class Particle{
+  constructor(x,y,radius){
+      var options = {
+          restitution: 0.5,
+          friction: 0
+      }
+      this.body = Bodies.circle(x,y,radius,options);
+      this.radius = radius;
+      World.add(world, this.body);
+  }
+  display(){
+      var pos = this.body.position;
+      push();
+      translate(pos.x,pos.y);
+      fill("white")
+      ellipse(0,0,this.radius*2);
+      pop();
+  }
+}
